b8540430251c8aa5c8f62aac06aa49f72414a0d5
Dennis <dennis.demmer1@gmail.com>
2024-06-18 09:48:58 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,10 +1,12 @@
 package de.demmer.dennis;
 
+import java.io.File;
+
 public class Application {
 
 	public static void main(String[] args) {
 		
-		
+		FileUtil.printDirectory(new File("C:\\Users\\Beispiel\\Documents"));
 		
 	}
 	
