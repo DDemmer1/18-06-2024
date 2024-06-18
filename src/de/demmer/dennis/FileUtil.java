206b41ef405af04ee1adcb595276f94cfa34e98c
Dennis <dennis.demmer1@gmail.com>
2024-06-18 09:45:01 +0200

    initial commit

--- src/de/demmer/dennis/FileUtil.java
+++ src/de/demmer/dennis/FileUtil.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class FileUtil {
 
-	public void printDirectory(File directory) {
+	public static void printDirectory(File directory) {
 		//TODO print all Files in directory 
 	}
 	
