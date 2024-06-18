e4944fb19e8e2553dd4011e2a1d474dcaf2923f3
Dennis <dennis.demmer1@gmail.com>
2024-06-18 11:07:51 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -7,22 +7,23 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		FileUtil.printDirectory(new File("C:\\Users\\Beispiel\\Documents"));
-		
-		// Erzeugt ein File Objekt mit einem Dateipfad im Parameter
-		File file = new File("C:\\Users\\Unknown\\git\\18-06-2024\\ordner1");
-		
-		//Überprüft ob das File existiert -> true/false
-		System.out.println(file.exists());
-		
-		//Überprüft ob das File ein Ordner ist -> true/false
-		System.out.println(file.isDirectory());
-		
-		//Listet alle Files in einem Ordner in einem Array auf
-		File[] fileArray = file.listFiles();
-		
-		//Gibt den genauen Pfad des Files als String zurück
-		System.out.println(file.getAbsolutePath());
+		String path = FileUtil.printDirectory(new File("C:\\Users\\Unknown\\git\\18-06-2024\\ordner1"), "text3.txt");
+		System.out.println(path);
+//		
+//		// Erzeugt ein File Objekt mit einem Dateipfad im Parameter
+//		File file = new File("C:\\Users\\Unknown\\git\\18-06-2024\\ordner1");
+//		
+//		//Überprüft ob das File existiert -> true/false
+//		System.out.println(file.exists());
+//		
+//		//Überprüft ob das File ein Ordner ist -> true/false
+//		System.out.println(file.isDirectory());
+//		
+//		//Listet alle Files in einem Ordner in einem Array auf
+//		File[] fileArray = file.listFiles();
+//		
+//		//Gibt den genauen Pfad des Files als String zurück
+//		System.out.println(file.getAbsolutePath());
 		
 
 	}
--- src/de/demmer/dennis/FileUtil.java
+++ src/de/demmer/dennis/FileUtil.java
@@ -3,10 +3,33 @@ package de.demmer.dennis;
 import java.io.File;
 
 public class FileUtil {
+	
+	static String result = "";
+													    // "text3.txt"
+	public static String printDirectory(File directory, String search) {
+		
+		if(!directory.isDirectory()) {
+			System.out.println("Is not a directory");
+			return "File not found";
+		}
+		
+		for (File file : directory.listFiles()) {
+			
+//			System.out.println(file.getAbsolutePath());
+			
+			if(file.getName().equals(search)) {
+				result = file.getAbsolutePath();
+			}
+			
+			if(file.isDirectory()) {
+				printDirectory(file, search);
+			}
+		}
 
-	public static void printDirectory(File directory) {
-		// TODO print all Files in directory
+		
+		return result;
+	
 	}
-
+	
 
 }
